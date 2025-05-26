a273a5b49a29e1574d5388ee7659c686e81e916f
Fran <shiro54828@gmail.com>
2025-05-26 10:38:52 -0600

    ultimos cambios

--- pruebaDeAbstraccion.java
+++ pruebaDeAbstraccion.java
@@ -1,6 +1,24 @@
 public class pruebaDeAbstraccion {
     public static void main(String[] args) {
-        
+        Futbolista futbolista = new Futbolista(1, "Lionel", "Messi", 34, 10, "Delantero");
+        futbolista.concentrarse();
+        futbolista.viajar();
+        futbolista.jugarPartido();
+        futbolista.entrenar();
+
+        System.out.println("--------------------------------");
+        Entrenador entrenador = new Entrenador(2, "Pep", "Guardiola", 50, "12345");
+        entrenador.concentrarse();
+        entrenador.viajar();
+        entrenador.dirigirPartido();
+        entrenador.dirigirEntrenamiento();
+        System.out.println("--------------------------------");
+
+        Masajista masajista = new Masajista(3, "Juan", "Pérez", 40, "Fisioterapia", 15);
+        masajista.concentrarse();
+        masajista.viajar();
+        masajista.darMasaje();
+        System.out.println("--------------------------------");
     }
 
     // Clase abstracta MiembroSeleccion
@@ -19,7 +37,7 @@ public class pruebaDeAbstraccion {
         public abstract void concentrarse();
         public abstract void viajar();
     }
-    class Futbolista extends MiembroSeleccion {
+    static class Futbolista extends MiembroSeleccion {
         private int dorsal;
         private String demarcacion;
     
@@ -47,7 +65,7 @@ public class pruebaDeAbstraccion {
         }
     }
     //clase Entrenador
-    class Entrenador extends MiembroSeleccion {
+    static class Entrenador extends MiembroSeleccion {
         private String idFederacion;
     
         public Entrenador(int id, String nombre, String apellidos, int edad, String idFederacion) {
@@ -69,6 +87,31 @@ public class pruebaDeAbstraccion {
         }
         public void dirigirEntrenamiento() {
             System.out.println("El entrenador " + nombre + " dirige el entrenamiento.");
+        }
+    }
+    // Clase Masajista
+    static class Masajista extends MiembroSeleccion {
+        private String titulacion;
+        private int aniosExperiencia;
+    
+        public Masajista(int id, String nombre, String apellidos, int edad, String titulacion, int aniosExperiencia) {
+            super(id, nombre, apellidos, edad);
+            this.titulacion = titulacion;
+            this.aniosExperiencia = aniosExperiencia;
+        }
+    
+        @Override
+        public void concentrarse() {
+            System.out.println("El masajista " + nombre + " se concentra.");
+        }
+    
+        @Override
+        public void viajar() {
+            System.out.println("El masajista " + nombre + " viaja con el equipo.");
+        }
+    
+        public void darMasaje() {
+            System.out.println("El masajista " + nombre + " da un masaje a un jugador.");
         }
     }
 }
