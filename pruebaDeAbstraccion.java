d15ca9399b49553da02cffe3b99dc54f778e3ea8
Fran <shiro54828@gmail.com>
2025-05-26 10:09:37 -0600

    Inicio del ejercicio

---
+++ pruebaDeAbstraccion.java
@@ -0,0 +1,11 @@
+public class pruebaDeAbstraccion {
+    public static void main(String[] args) {
+
+    }
+}
+class MiembroSeleccion {
+    protected int id;
+    protected String nombre;
+    protected String apellidos;
+    protected int edad;
+}
